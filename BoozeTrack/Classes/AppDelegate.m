11e729833296038f026337cc4153d1300208fb72
Chris Petersen <geek@ex-nerd.com>
2012-06-07 01:11:52 -0700

    move www to boozetrack-cordova project

--- BoozeTrack/Classes/AppDelegate.m
+++ BoozeTrack/Classes/AppDelegate.m
@@ -77,7 +77,7 @@
     
     self.viewController = [[[MainViewController alloc] init] autorelease];
     self.viewController.useSplashScreen = YES;
-    self.viewController.wwwFolderName = @"www";
+    self.viewController.wwwFolderName = @"cordova";
     self.viewController.startPage = @"index.html";
     self.viewController.invokeString = invokeString;
     self.viewController.view.frame = viewBounds;
